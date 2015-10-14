5ffb0ca22d89db600d8aeeed7f9b5ca297ad52fe
wingsoverglades <wingsoverglades@gmail.com>
2015-10-14 15:09:59 -0400

    added TODO's

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,10 @@
 fn main()
 {
-        
+        //TODO start menu where the user can choose their projects and settings (IDE style)
+        /*
+         *Certain variables that we get from the user's actions in the start menu
+         */
+
+        //TODO Begin the screen where the user makes the mechanism
+        //TODO save the user's work
 }
